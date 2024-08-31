b084f6ee15d3d4e8a360458f6d76ce5e21233280
Derrick Yew <yewcm123@gmail.com>
2024-09-01 01:01:30 +0800

    added throttler guard

--- src/1.flight/flight.module.ts
+++ src/1.flight/flight.module.ts
@@ -2,10 +2,18 @@ import { Module } from '@nestjs/common';
 import { FlightController } from './flight.controller';
 import { FlightService } from './flight.service';
 import { HttpModule } from '@nestjs/axios';
+import { APP_GUARD } from '@nestjs/core';
+import { ThrottlerGuard } from '@nestjs/throttler';
 
 @Module({
-  imports: [HttpModule],
-  controllers: [FlightController],
-  providers: [FlightService],
+    imports: [HttpModule],
+    controllers: [FlightController],
+    providers: [
+        FlightService,
+        {
+            provide: APP_GUARD,
+            useClass: ThrottlerGuard,
+        },
+    ],
 })
-export class FlightModule { }
+export class FlightModule {}
--- src/app.module.ts
+++ src/app.module.ts
@@ -3,15 +3,29 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { FlightModule } from './1.flight/flight.module';
 import { ConfigModule } from '@nestjs/config';
+import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler';
+import { APP_GUARD } from '@nestjs/core/constants';
 
 @Module({
     imports: [
-        FlightModule,
         ConfigModule.forRoot({
             isGlobal: true,
         }),
+        ThrottlerModule.forRoot([
+            {
+                ttl: 60,
+                limit: 10,
+            },
+        ]),
+        FlightModule,
     ],
     controllers: [AppController],
-    providers: [AppService],
+    providers: [
+        AppService,
+        {
+            provide: APP_GUARD,
+            useClass: ThrottlerGuard,
+        },
+    ],
 })
 export class AppModule {}
